33df71cd1b4489f5b2a83921f20ab341036434ea
Kristen Lee <kristen.lee.3553@gmail.com>
2026-02-08 00:22:33 -0500

    thanks page mobile

--- src/pages/thanks-vote/ThanksVote.tsx
+++ src/pages/thanks-vote/ThanksVote.tsx
@@ -1,25 +1,22 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import styles from "./ThanksVote.module.css";
-import { CircularProgress, IconButton, Typography } from "@mui/material";
+import { IconButton } from "@mui/material";
 import CheckIcon from "../../images/check.svg?react";
 import Back from "../../images/back.svg?react";
 
-
-// CYNTHIA THIS IS TEMP IDK WHAT THE DB STATES ARE
-type RoomState = "choice" | "rank";
-
-// EVERYONE LANDS HERE BUT GETS REDIRECTED BASED ON ROOM STATE CYNTHIA
 export default function ThanksVotePage() {
   const { roomId } = useParams();
   const navigate = useNavigate();
   const [loading, setLoading] = useState<boolean>(true);
+  const [topic, setTopic] = useState<string>("");
 
-//help me cynthia
+  //help me cynthia
   useEffect(() => {
     try {
       if (!roomId) return;
-      const roomState: RoomState = "choice";
+      // CYNTHIA TOPIC
+      setTopic("TOPIC");
     } catch (e) {
       console.error(e);
     } finally {
@@ -27,28 +24,27 @@ export default function ThanksVotePage() {
     }
   }, [roomId]);
 
-  const handleOnBack = () => {};
-  
+  const handleOnBack = () => {
+    navigate("/");
+  };
+
   return (
     <div className={styles.thanksVoteContainer}>
-        <div className={styles.titleWrapper}>
-            <IconButton onClick={handleOnBack} className={styles.backButton}>
-                <Back />
-                </IconButton>
-        </div>
-    <CheckIcon className={styles.check} />
-    <div className={styles.contentContainer}>
+      <div className={styles.titleWrapper}>
+        <IconButton onClick={handleOnBack} className={styles.backButton}>
+          <Back />
+        </IconButton>
+      </div>
+      <CheckIcon className={styles.check} />
+      <div className={styles.contentContainer}>
         Thanks for voting
-        <div className={styles.topic}> 
-            Topic
-        </div>
+        <div className={styles.topic}>{topic}</div>
         <div className={styles.waitMessage}>
-            Please wait while we tally the results...
-            <br/>
-            Come back after host confirmation!
+          Please wait while we tally the results...
+          <br />
+          Come back after host confirmation!
         </div>
-    </div>
-    
+      </div>
     </div>
   );
 }
